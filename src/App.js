efffc0165564e87d39029a7bb0cdfcb0fee46f8b
Kim Pham <pham.kim.a@gmail.com>
2019-02-04 12:35:10 -0600

    delete user components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import '../node_modules/semantic-ui-css/semantic.css'
 
 import PrivateRoute from './components/PrivateRoute'
 import LoginContainer from './components/LoginContainer'
-import UserContainer from './components/UserContainer'
 import ExploreContainer from './components/ExploreContainer'
 import NotFoundContainer from './components/NotFoundContainer'
 import OtherUserContainer from './components/OtherUserContainer'
@@ -41,7 +40,6 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={LoginContainer} />
-          {/* <PrivateRoute exact path={`/${this.props.user.username}`} component={UserContainer} /> */}
           <PrivateRoute exact path={`/explore`} component={ExploreContainer} />
           <PrivateRoute exact path="/places/:city" component={DestinationContainer} />
           <PrivateRoute exact path="/404" component={NotFoundContainer} />
